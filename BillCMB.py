16d1f8a6d2a415bdfb29237ca51b28c1912017f0
Sam <anzic2008@gmail.com>
2019-12-07 14:29:14 +0800

    Suport CMB & COMM

---
+++ BillCMB.py
@@ -0,0 +1,63 @@
+# encoding: utf-8
+from Bill import Bitem
+from Bill import Bill
+
+from datetime import datetime
+
+import quopri
+import re
+from warnings import warn
+
+class BillCMB(Bill):
+
+    def parse(self, fname):
+        'parse bill content from eml(fname)'
+
+        f = open(fname, 'rb')
+        eml_bytes = f.read()
+        f.close()
+
+        # quoted-printable decode
+        quo_dec = quopri.decodestring(eml_bytes)
+        quo_dec = quo_dec.decode('utf-8')
+
+        self.parse_year(quo_dec)
+        self.parse_contents(quo_dec)
+    
+    def parse_year(self, quo_dec):
+        pattern = re.compile(r'zdzq.jpg.*?(\d{4})/\d{2}/\d{2}-\d{4}/\d{2}/\d{2}', re.S)
+        res = pattern.findall(quo_dec)[0]
+        self.year = res
+
+    def parse_contents(self, quo_dec):
+        pattern = re.compile(r'rmbbt.jpg(.*?)★', re.S)
+        res = pattern.findall(quo_dec)[0]
+        pattern = re.compile(r'<font face="宋体" style="font-size:12px;line-height:120%;">(.*?)</font></div>')
+        res = pattern.findall(res)
+
+        if len(res) % 7 != 0:
+            warn('tabel numbers mismatch!')
+        
+        for i in range(0, len(res)//7):
+            target = res[i*7:(i+1)*7]
+            
+            time = datetime.strptime(self.year+target[0], '%Y%m%d')
+            des = target[2]
+            
+            amount = amount = target[3] # ￥&nbsp;54.99
+            amount = amount.replace('￥', '')
+            amount = amount.replace('&nbsp', '')
+            amount = amount.replace(';', '')
+            amount = amount.replace(' ', '')
+            amount = float(amount)
+            
+            bitem = Bitem(amount=amount, time=time, des=des, src='CMB')
+            self.conts.append(bitem)
+
+            pass
+
+if __name__ == '__main__':
+    bill = BillCMB()
+    bill.parse('./Bill/CMB-201910.eml')
+    bill.save()
+
---
+++ BillCOMM.py
@@ -0,0 +1,57 @@
+# encoding: utf-8
+from Bill import Bitem
+from Bill import Bill
+
+from datetime import datetime
+
+import base64
+import re
+from warnings import warn
+
+class BillCOMM(Bill):
+
+    def parse(self, fname):
+        'parse bill content from eml(fname)'
+
+        f = open(fname, 'rt')
+        eml = f.read()
+        f.close()
+
+        pattern = re.compile(r'\n\n(.*)', re.S)
+        eml = pattern.findall(eml)[0]
+        eml = base64.b64decode(eml)
+        eml = eml.decode('gbk')
+
+        pattern = re.compile(r'还款、退货、费用返还明细(.*?)消费、取现、其他费用明细(.*)', re.S)
+        (eml_income, eml_expense) = pattern.findall(eml)[0]
+
+        self.parse_content(eml_income, 1)
+        self.parse_content(eml_expense, -1)
+    
+    def parse_content(self, eml_str, sign):
+        pattern = re.compile(r'''<td style="font-family: 'Hiragino Sans GB'"><span>(.*?)</span></td>''')
+        res = pattern.findall(eml_str)
+
+        if len(res) % 6 != 0:
+            warn('tabel numbers mismatch!')
+        
+        for i in range(0, len(res)//6):
+            target = res[i*6:(i+1)*6]
+            
+            time = datetime.strptime(target[0], '%Y/%m/%d')
+            des = target[3]
+        
+            amount = amount = target[4] # RMB 1.50
+            amount = amount.replace('RMB', '')
+            amount = amount.replace(' ', '')
+            amount = float(amount)*sign
+            
+            bitem = Bitem(amount=amount, time=time, des=des, src='COMM')
+            self.conts.append(bitem)
+
+
+if __name__ == '__main__':
+    bill = BillCOMM()
+    bill.parse('./Bill/COMM-201909.eml')
+    bill.save()
+
